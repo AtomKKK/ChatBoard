835001500ea2f3fe046ccc84408799d770b0852c
zhan jun <zhanjun@outlook.com>
2016-12-13 11:43:07 +0800

    Initial commit

---
+++ app/src/main/java/com/qtking/chatboard/MainActivity.java
@@ -0,0 +1,41 @@
+package com.qtking.chatboard;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toobar)
+    Toolbar mToolbar;
+    @BindView(R.id.btn_send)
+    Button mButton;
+    @BindView(R.id.et_msg)
+    EditText mEditText;
+    @BindView(R.id.rv)
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        mToolbar.setTitle("猪妹");
+
+
+    }
+}
